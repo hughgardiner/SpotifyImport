2625876dd682e84ace1c0464ab5a727095e6b383
Hugh Gardiner <gardinerhugh6@gmail.com>
2019-08-18 23:53:58 -0400

    use store_true parameter for genre param

--- spotify_import.py
+++ spotify_import.py
@@ -8,7 +8,7 @@ parser = argparse.ArgumentParser(description='Import songs to Spotify from Textf
 parser.add_argument('username', type=str, help='Spotify Username')
 parser.add_argument('filename', type=str, help='Name of Textfile for import')
 parser.add_argument('-p', '--playlist', type=str, help='Name of playlist to import songs into')
-parser.add_argument('-g', '--genre', type=str, help='Include yaml file categorizing subgenres into playlists')
+parser.add_argument('-g', '--genre', action='store_true', help='Include yaml file categorizing subgenres into playlists')
 parser.add_argument('-b', '--batch-size', type=int, help='Number of songs per Spotify API Request')
 args = parser.parse_args()
 

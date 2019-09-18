efe0cc2c9116417c8b5acae655522ed6099e81cc
Hugh Gardiner <gardinerhugh6@gmail.com>
2019-09-17 22:31:47 -0500

    implement intelligent retry; used formatter and updated to 4 spaces

--- spotify_import.py
+++ spotify_import.py
@@ -5,31 +5,38 @@ import argparse
 import yaml
 from spotipy_helpers import *
 
-parser = argparse.ArgumentParser(description='Import songs to Spotify from Textfile')
+parser = argparse.ArgumentParser(
+    description='Import songs to Spotify from Textfile')
 parser.add_argument('username', type=str, help='Spotify Username')
 parser.add_argument('filename', type=str, help='Name of Textfile for import')
-parser.add_argument('-p', '--playlist', type=str, help='Name of playlist to import songs into')
-parser.add_argument('-g', '--genre', type=str, help='Include yaml file categorizing subgenres into playlists')
+parser.add_argument('-p', '--playlist', type=str,
+                    help='Name of playlist to import songs into')
+parser.add_argument('-g', '--genre', type=str,
+                    help='Include yaml file categorizing subgenres into playlists')
 args = parser.parse_args()
 
+
 def validate_inputs(args):
-  if args.genre is not None and args.playlist is not None:
-    print('Both playlist and genre flags cannot be specified')
-    sys.exit(1)
+    if args.genre is not None and args.playlist is not None:
+        print('Both playlist and genre flags cannot be specified')
+        sys.exit(1)
+
 
 def invalid_genre_file():
-  print('Genre yaml file is not a valid format')
-  sys.exit(1)
+    print('Genre yaml file is not a valid format')
+    sys.exit(1)
+
 
 # Pivots provided Playlist/Genre File for easier Playlist lookup
 def create_genre_playlist_dict(genre_file):
-  genre_playlist_dict = {}
-  for key, value in genre_file.items():
-    if type(value) is not list:
-      invalid_genre_file()
-    for item in value:
-      genre_playlist_dict[item] = key
-  return genre_playlist_dict
+    genre_playlist_dict = {}
+    for key, value in genre_file.items():
+        if type(value) is not list:
+            invalid_genre_file()
+        for item in value:
+            genre_playlist_dict[item] = key
+    return genre_playlist_dict
+
 
 validate_inputs(args)
 
@@ -37,19 +44,24 @@ sp = authenticate_spotipy(args.username)
 user_playlists = sp.user_playlists(args.username)['items']
 
 timestamp = datetime.datetime.now().strftime("%m_%d_%Y_%X")
-songs_not_found = open(f'output/songs_not_found_{timestamp}.csv',"w")
+songs_not_found = open(f'output/songs_not_found_{timestamp}.csv', "w")
 if args.genre is not None:
-  playlist_hash = {}
-  with open(args.genre, 'r') as genre_file:
-    playlists_by_genre = create_genre_playlist_dict(yaml.safe_load(genre_file))
-    if type(playlists_by_genre) is not dict:
-      invalid_genre_file()
-    playlist_hash = parse_playlists_from_genre(sp, args.username, user_playlists, playlists_by_genre)
-  if playlist_hash:
-    add_songs_from_csv_genre(sp, args.filename, args.username, playlists_by_genre, playlist_hash, songs_not_found)
+    playlist_hash = {}
+    with open(args.genre, 'r') as genre_file:
+        playlists_by_genre = create_genre_playlist_dict(
+            yaml.safe_load(genre_file))
+        if type(playlists_by_genre) is not dict:
+            invalid_genre_file()
+        playlist_hash = parse_playlists_from_genre(
+            sp, args.username, user_playlists, playlists_by_genre)
+    if playlist_hash:
+        add_songs_from_csv_genre(sp, args.filename, args.username,
+                                 playlists_by_genre, playlist_hash, songs_not_found)
 
 else:
-  playlist_id = find_or_create_playlist(sp, args.username, user_playlists, args.playlist)
-  add_songs_from_csv(sp, args.filename, args.username, playlist_id, songs_not_found)
+    playlist_id = find_or_create_playlist(
+        sp, args.username, user_playlists, args.playlist)
+    add_songs_from_csv(sp, args.filename, args.username,
+                       playlist_id, songs_not_found)
 
 songs_not_found.close()
--- spotipy_helpers.py
+++ spotipy_helpers.py
@@ -4,105 +4,123 @@ import spotipy
 import spotipy.util as util
 import json
 
-SPOTIPY_CLIENT_ID='4b8925015d0b47d9b9bf719ebe5cf882'
-SPOTIPY_CLIENT_SECRET='833d5ca3bdab45a0b7713c13aa1733ee'
-SPOTIPY_REDIRECT_URI='http://localhost/'
+SPOTIPY_CLIENT_ID = '4b8925015d0b47d9b9bf719ebe5cf882'
+SPOTIPY_CLIENT_SECRET = '833d5ca3bdab45a0b7713c13aa1733ee'
+SPOTIPY_REDIRECT_URI = 'http://localhost/'
+
 
 def authenticate_spotipy(username):
-  scope = 'playlist-modify-public'
-  token = util.prompt_for_user_token(username, scope,client_id=SPOTIPY_CLIENT_ID,client_secret=SPOTIPY_CLIENT_SECRET,redirect_uri=SPOTIPY_REDIRECT_URI)
-  if token:
-    return spotipy.Spotify(auth=token)
-  else:
-    print('Authentication to Spotify Failed')
-    raise
+    scope = 'playlist-modify-public'
+    token = util.prompt_for_user_token(username, scope, client_id=SPOTIPY_CLIENT_ID,
+                                       client_secret=SPOTIPY_CLIENT_SECRET, redirect_uri=SPOTIPY_REDIRECT_URI)
+    if token:
+        return spotipy.Spotify(auth=token)
+    else:
+        print('Authentication to Spotify Failed')
+        raise
+
 
 def find_or_create_playlist(sp, username, user_playlists, playlist_name):
-  playlist_name = playlist_name or f'Spotify Import {datetime.datetime.now().strftime("%b %d %Y")}'
-  playlistfound = list(filter(lambda d: d['name'] in [f'{playlist_name}'], user_playlists))
-  if not playlistfound:
-    return sp.user_playlist_create(username, playlist_name)['id']
-  else:
-    return playlistfound[0]['id']
+    playlist_name = playlist_name or f'Spotify Import {datetime.datetime.now().strftime("%b %d %Y")}'
+    playlistfound = list(filter(lambda d: d['name'] in [
+                         f'{playlist_name}'], user_playlists))
+    if not playlistfound:
+        return sp.user_playlist_create(username, playlist_name)['id']
+    else:
+        return playlistfound[0]['id']
+
 
 def add_songs_from_csv(sp, csv_file, username, playlist_id, songs_not_found):
-  with open(csv_file, newline='') as csv_file:
-    song_reader = csv.reader(csv_file)
-    counter = 0
-    song_ids = []
-    for row in song_reader:
-      response = find_song(sp, row[0], row[1])  
-      if response:
-        counter += 1
-        first_song = response[0]
-        song_ids.append(first_song['id'])
-        print("Found {0} by {1}".format(first_song['name'], first_song['album']['artists'][0]['name']))
-        if counter % 100 == 0:
-          sp.user_playlist_add_tracks(username, playlist_id, song_ids)
-          song_ids = []
-          counter = 0
-      else:
-        songs_not_found.write(f"{row[0]},{row[1]},{row[2]}\r\n")
-    sp.user_playlist_add_tracks(username, playlist_id, song_ids)
+    with open(csv_file, newline='') as csv_file:
+        song_reader = csv.reader(csv_file)
+        counter = 0
+        song_ids = []
+        for row in song_reader:
+            response = find_song(sp, row[0], row[1])
+            if response:
+                counter += 1
+                first_song = response[0]
+                song_ids.append(first_song['id'])
+                print("Found {0} by {1}".format(
+                    first_song['name'], first_song['album']['artists'][0]['name']))
+                if counter % 100 == 0:
+                    sp.user_playlist_add_tracks(
+                        username, playlist_id, song_ids)
+                    song_ids = []
+                    counter = 0
+            else:
+                songs_not_found.write(f"{row[0]},{row[1]},{row[2]}\r\n")
+        sp.user_playlist_add_tracks(username, playlist_id, song_ids)
+
 
 def add_songs_from_csv_genre(sp, csv_file, username, playlists_by_genre, playlist_hash, songs_not_found):
-  with open(csv_file, newline='') as csv_file:
-    song_reader = csv.reader(csv_file)
-    for row in song_reader:
-      response = find_song(sp, row[0], row[1])  
-      if response:
-        playlist_key = playlists_by_genre[row[2]] if row[2] in playlists_by_genre else None
-        if playlist_key:
-          first_song = response[0]
-          playlist_hash[playlist_key]['song_ids'].append(first_song['id'])
-          print("Found {0} by {1} for playlist {2}".format(first_song['name'], first_song['album']['artists'][0]['name'], playlist_key))
-          if len(playlist_hash[playlist_key]['song_ids']) % 100 == 0:
-            sp.user_playlist_add_tracks(username, playlist_hash[playlist_key]['playlist_id'], playlist_hash[playlist_key]['song_ids'])
-            playlist_hash[playlist_key]['song_ids'] = []
-        else:
-          print(f'Could not associate playlist for genre {row[2]}. Song {row[0]} - {row[1]} not added to a playlist')
-          songs_not_found.write(f"{row[0]},{row[1]},{row[2]}\r\n")
-      else:
-        songs_not_found.write(f"{row[0]},{row[1]},{row[2]}\r\n")
-    for playlist in playlist_hash:
-      if len(playlist_hash[playlist]['song_ids']) > 0:
-        sp.user_playlist_add_tracks(username, playlist_hash[playlist]['playlist_id'], playlist_hash[playlist]['song_ids'])
+    with open(csv_file, newline='') as csv_file:
+        song_reader = csv.reader(csv_file)
+        for row in song_reader:
+            response = find_song(sp, row[0], row[1])
+            if response:
+                playlist_key = playlists_by_genre[row[2]
+                                                  ] if row[2] in playlists_by_genre else None
+                if playlist_key:
+                    first_song = response[0]
+                    playlist_hash[playlist_key]['song_ids'].append(
+                        first_song['id'])
+                    print("Found {0} by {1} for playlist {2}".format(
+                        first_song['name'], first_song['album']['artists'][0]['name'], playlist_key))
+                    if len(playlist_hash[playlist_key]['song_ids']) % 100 == 0:
+                        sp.user_playlist_add_tracks(
+                            username, playlist_hash[playlist_key]['playlist_id'], playlist_hash[playlist_key]['song_ids'])
+                        playlist_hash[playlist_key]['song_ids'] = []
+                else:
+                    print(
+                        f'Could not associate playlist for genre {row[2]}. Song {row[0]} - {row[1]} not added to a playlist')
+                    songs_not_found.write(f"{row[0]},{row[1]},{row[2]}\r\n")
+            else:
+                songs_not_found.write(f"{row[0]},{row[1]},{row[2]}\r\n")
+        for playlist in playlist_hash:
+            if len(playlist_hash[playlist]['song_ids']) > 0:
+                sp.user_playlist_add_tracks(
+                    username, playlist_hash[playlist]['playlist_id'], playlist_hash[playlist]['song_ids'])
 
 
 def find_song(sp, songname, artist):
-  try:
-    query = f'artist:%{artist} track:%{songname}'
-    songs = sp.search(q=query, type='track')['tracks']['items'] 
-    if songs:
-      return songs
-    else:
-      query = f'artist:%{simply_search_query(artist)} track:%{simply_search_query(songname)}'
-      return sp.search(q=query, type='track')['tracks']['items']
-  except spotipy.client.SpotifyException as err: 
-    print(f'Spotipy Exception occurred on track{songname} - {artist}')
-    return None
+    try:
+        query = f'artist:%{artist} track:%{songname}'
+        songs = sp.search(q=query, type='track')['tracks']['items']
+        if songs:
+            return songs
+        else:
+            query = f'artist:%{simply_search_query(artist)} track:%{simply_search_query(songname)}'
+            return sp.search(q=query, type='track')['tracks']['items']
+    except spotipy.client.SpotifyException as err:
+        print(f'Spotipy Exception occurred on track{songname} - {artist}')
+        return None
+
 
 def simply_search_query(text):
-  plain_text = text.lower().strip()
-  if '(' in plain_text:
-    plain_text = plain_text.split('(')[0]
-  if 'feat' in plain_text:
-    plain_text = plain_text.split('feat')[0]
-  if 'ft' in plain_text:
-    plain_text = plain_text.split('ft')[0]
-  if 'vs' in plain_text:
-    plain_text = plain_text.split('vs')[0]
-  if 'and' in plain_text:
-    plain_text = plain_text.split('and')[0]
-  if '&' in plain_text:
-    plain_text = plain_text.split('&')[0]
-  if ' x ' in plain_text:
-    plain_text = plain_text.split(' x ')[0]
-  return plain_text
+    plain_text = text.lower().strip()
+    if '(' in plain_text:
+        plain_text = plain_text.split('(')[0]
+    if 'feat' in plain_text:
+        plain_text = plain_text.split('feat')[0]
+    if 'ft' in plain_text:
+        plain_text = plain_text.split('ft')[0]
+    if 'vs' in plain_text:
+        plain_text = plain_text.split('vs')[0]
+    if 'and' in plain_text:
+        plain_text = plain_text.split('and')[0]
+    if '&' in plain_text:
+        plain_text = plain_text.split('&')[0]
+    if ' x ' in plain_text:
+        plain_text = plain_text.split(' x ')[0]
+    return plain_text
+
 
 def parse_playlists_from_genre(sp, username, user_playlists, playlists_by_genre):
     playlists = set(val for val in playlists_by_genre.values())
-    playlist_hash = { playlist: { "playlist_id": None, "song_ids": [] } for playlist in playlists }
+    playlist_hash = {playlist: {"playlist_id": None, "song_ids": []}
+                     for playlist in playlists}
     for playlist in playlist_hash.keys():
-      playlist_hash[playlist]['playlist_id'] = find_or_create_playlist(sp, username, user_playlists, playlist)
+        playlist_hash[playlist]['playlist_id'] = find_or_create_playlist(
+            sp, username, user_playlists, playlist)
     return playlist_hash
